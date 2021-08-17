e2537b8acf1c550f2b157c4ae531d0c720f3c891
Lihue Luna <lihue.luna@mi.unc.edu.ar>
2021-08-17 15:42:06 -0300

    meminfo ready

--- meminfo.c
+++ meminfo.c
@@ -1,20 +1,42 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "myFuncs.h"
 
 void meminfo(void){
-
+    int totalFound=0,availableFound=0,freeFound=0;
+    
     struct Meminfo{
         int total;
         int free;
         int available;
         int swap;
-    };
+    }meminfo;
 
     char buffer[100];
 
-    FILE* info = openFile("/proc/meminfo");
+    FILE* file = openFile("/proc/meminfo");
 
-    fgets(buffer,100,info);
-    printf("%s",buffer);
+    while(fgets(buffer,100,file)!=NULL){
+        if(strstr(buffer,"MemTotal:") && !totalFound){
+            meminfo.total=atoi(strtok(strchr(buffer,' ')," "));
+            totalFound=1;
+        }
+        if(strstr(buffer,"MemFree:")&& !availableFound){
+            meminfo.free=atoi(strtok(strchr(buffer,' ')," "));
+            availableFound=1;
+        }
+        if(strstr(buffer,"MemAvailable:")&& !freeFound){
+            meminfo.available=atoi(strtok(strchr(buffer,' ')," "));
+            freeFound=1;
+        }
+        if(totalFound && availableFound && freeFound){
+            break;
+        }
+    }
+    meminfo.swap=meminfo.total-meminfo.free;
+    printf("Memoria Total: %d Mb\n",meminfo.total/1000);
+    printf("Memoria Libre: %d Mb\n",meminfo.free/1000);
+    printf("Memoria Disponible: %d Mb\n",meminfo.available/1000);
+    printf("Memoria Ocupada: %d Mb\n",meminfo.swap/1000);
 }
